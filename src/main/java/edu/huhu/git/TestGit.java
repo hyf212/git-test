c59807437b4c4da723bd24534aa64695d507353d
huangyanfeng <3513085038@qq.com>
2024-12-11 14:34:02 +0800

    second commit

--- src/main/java/edu/huhu/git/TestGit.java
+++ src/main/java/edu/huhu/git/TestGit.java
@@ -9,5 +9,6 @@ package edu.huhu.git;
 public class TestGit {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git2");
     }
 }
